84196a8c51268310fa1df6c0a415f2f6d2311692
stefan911 <danielstefan.halasz@gmail.com>
2021-10-17 18:18:47 +0300

    Prettified greedy_10.h

--- probleme/others/greedy/greedy_10.h
+++ probleme/others/greedy/greedy_10.h
@@ -8,30 +8,30 @@
 #include "utils/FileAPI.h"
 #include "utils/Arrays.h"
 #include <algorithm>
+
 #define FILE_NAME "bac"
 #define CONTENT_ROOT "C:\\Users\\dmihn\\CLionProjects\\CUtilProblems\\input-files\\greedy\\greedy-10\\"
 
 void solve() {
     setInputFolder(CONTENT_ROOT);
     initialiseFiles(FILE_NAME);
-    size_t n;
-    auto *arr = readArray<int>(n, file_in);
-    sort(arr, arr + n / 2);
+
+    size_t length;
+    auto *arr = readArray<int>(length, file_in);
+    sort(arr, arr + length / 2);
+
     int max = -1;
-    for (int i = n / 2; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    if (arr[n / 2 - 1] <= max) {
+    for (int i = length / 2; i < length; i++) if (arr[i] > max) max = arr[i];
+
+    if (arr[length / 2 - 1] <= max) {
         writeToFile("Nu exista");
-    } else {
-        for (int i = 0; i < n / 2; i++) {
-            if (arr[i] > max) {
-                writeToFile(arr[i]);
-                break;
-            }
-        }
+        return;
+    }
+
+    for (int i = 0; i < length / 2; i++) {
+        if (arr[i] <= max) continue;
+        writeToFile(arr[i]);
+        return;
     }
 }
 
